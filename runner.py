6ab6299d05c71fcc3be941d5244db6de7a1eed7c
Bogna Knychala <klucha.leniwa@gmail.com>
2013-04-09 00:24:49 +0200

    Get info about fps from avi file and frames from txt file

--- runner.py
+++ runner.py
@@ -10,6 +10,8 @@ def main():
     """Main function - runs program and parses arguments"""
 
     parser = argparse.ArgumentParser('Convert txt file to srt file')
+    media_parser = parser.add_mutually_exclusive_group(required=True)
+
     parser.add_argument('-i', '--input_txt_file', dest='text_file',
                         action='store', required=True,
                         help='Path to the text file you want to \
@@ -17,14 +19,19 @@ def main():
     parser.add_argument('-o', '--output_file', dest='output_file',
                         action='store', required=False,
                         help='Optional path to the output srt file, \
-                        by default current path')
+                        by default current path, file name same as txt file')
 
-    args = parser.parse_args()
+    media_parser.add_argument('--movie', dest='movie', action='store_true')
+    media_parser.add_argument('--fps', action='store_true')
+
+    args, unknown = parser.parse_known_args()
 
     if args.text_file:
         file_parser = Text_file_parser(text_file=args.text_file)
         file_parser.open_file()
         file_parser.check_text_file()
+        file_parser.get_frames_from_txt_file()
+        file_parser.get_frames_per_second(unknown[0])
 
 
 
--- text_file_parser.py
+++ text_file_parser.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-import mimetypes
+
+from mimetypes import guess_type
+from subprocess import call, Popen, PIPE
 from FileParsingErrors import BadTypeException
 
 __author__ = 'bogna'
@@ -23,11 +25,34 @@ class Text_file_parser():
 
     def check_text_file(self):
         """Check if loaded file is a correct text file"""
-        if not self.expected_format in mimetypes.guess_type(self.text_file):
+        if not self.expected_format in guess_type(self.text_file):
             raise BadTypeException(
                 'Expected format was {0} and format found was {1}'.format(
                     self.expected_format,
-                    mimetypes.guess_type(self.text_file)[0]))
+                    guess_type(self.text_file)[0]))
+
+    def get_frames_from_txt_file(self):
+        """Extract frames for each subtitle from text file"""
+        self.frames = []
+        for line in self.opened_file.readlines():
+            self.frames.append(line.translate(None, "{").split("}")[:2])
+
+    def get_frames_per_second(self, movie_file):
+        """Get frame rate using mediainfo utility"""
+        process = Popen(['mediainfo', movie_file], stdout=PIPE)
+        result = process.communicate()[0].split("\n")
+        fps = None
+        for line in result:
+            if "Frame rate" in line:
+                fps = line.split(":")[1].translate(None, "")
+        self.fps = fps.strip().split(" ")[0]
+
+    def convert_from_frames_to_millis(self):
+        """Using info about frame rate and frames extracted from text file
+        convert frames to milliseconds"""
+
+
+
 
 
 if __name__ == '__main__':
